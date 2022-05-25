ab09676d45e9bdce636adfb1e26be5641e62cedf
XYZ <haunt.hcm2015@gmail.com>
2022-05-25 08:44:37 +0700

    Create smile.py

---
+++ smile.py
@@ -0,0 +1,85 @@
+from turtle import *
+
+setup(960, 640)
+
+
+pensize(200)
+
+pencolor("yellow")
+
+circle(100, 360)
+
+
+penup()
+
+seth(90)
+
+fd(90)
+
+seth(180)
+
+fd(150)
+
+seth(270)
+
+pendown()
+
+pensize(5)
+
+pencolor("black")
+
+circle(150, 180)
+
+
+penup()
+
+fd(90)
+
+seth(155)
+
+pendown()
+
+pensize(40)
+
+pencolor("white")
+
+circle(160, 45)
+
+penup()
+
+circle(160, -45)
+
+seth(180)
+
+fd(180)
+
+seth(155)
+
+pendown()
+
+circle(160, 45)
+
+
+penup()
+
+seth(0)
+
+fd(20)
+
+pendown()
+
+pensize(16)
+
+pencolor("black")
+
+circle(8)
+
+penup()
+
+fd(180)
+
+pendown()
+
+circle(8)
+
+done()
